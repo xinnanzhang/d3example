7b3d3de57596bcc1889c9b5aadb45b4215f8e31d
seaove <seaove@gmail.com>
2016-05-31 15:35:12 +0800

    update

--- example/src/app/example/exampleB.js
+++ example/src/app/example/exampleB.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+//函数拓扑图
 var ExampleTopo = React.createClass({
   componentDidMount(){
     var _this = this;
@@ -63,6 +64,8 @@ var ExampleTopo = React.createClass({
     .on("mouseover",function(d,i){
       // alert();
       _this.startMove( this , 30 , 40 );
+      //找到对应的线
+
     })
     .on("mouseout",function(d,i){
       _this.startMove( this, 40 , 30 );
@@ -121,6 +124,198 @@ var ExampleTopo = React.createClass({
     )
   }
 });
+//按照顺序  填充拓扑图
+var OrderTopo = React.createClass({
+  getInitialState(){
+    return{
+      centerX:0,
+      centerY:0
+    }
+  },
+  addOtherTag(otherAttr,oSvg){
+    var _this = this;
+    let centerX = oSvg.attr("width")/2;
+    let centerY = oSvg.attr("height")/2;
+    //线  --- 圆  ---- 中心点
+
+    var oG = oSvg.selectAll("g").data(otherAttr).enter().append("g").attr("style","cursor:pointer");
+    //线 + 方块 + 文字
+    oG.append("line").attr("stroke","#ccc")
+    .attr("x1",function(d,i){
+      return d.x;
+    })
+    .attr("y1",function(d,i){
+      return d.y;
+    })
+    .attr("x2",centerX)
+    .attr("y2",centerY);
+
+    oG.append("rect").attr("fill","#999").attr("width",20).attr("height",20).attr("rx",5)
+    .attr("x",function(d,i){
+      return (d.x + centerX)/2 -10;
+    })
+    .attr("y",function(d,i){
+      return (d.y + centerY)/2 - 10;
+    });
+    oG.append("text").attr("fill","white").attr("font-size",20).attr("height",20).attr("text-anchor","middle")
+    .attr("x",function(d,i){
+      return (d.x + centerX)/2;
+    })
+    .attr("y",function(d,i){
+      return (d.y + centerY)/2 +8;
+    }).text("?");
+
+    //节点圆 + 文字
+    oG.append("circle")
+    .attr("fill","white").attr("stroke","red").attr("stroke-width","1")
+    .attr("cx",function(d,i){
+      return d.x;
+    })
+    .attr("cy",function(d,i){
+      return d.y;
+    })
+    .attr("r",25)
+    .on("mouseover",function(d,i){
+      _this.startMove( this , 30 , 40 );
+      //找到对应的线  parentNode返回当前的兄弟节点
+      this.parentNode.children[0].setAttribute('stroke','blue');
+      this.parentNode.children[1].setAttribute('fill','red');
+    })
+    .on("mouseout",function(d,i){
+      _this.startMove( this, 40 , 30 );
+      this.parentNode.children[0].setAttribute('stroke','#ccc');
+      this.parentNode.children[1].setAttribute('fill','#999');
+    });
+
+    oG.append("text")
+    .attr("font-size",20).attr("text-anchor","middle")
+    .attr("x",function(d,i){
+      return d.x;
+    })
+    .attr("y",function(d,i){
+      return d.y+8;
+    })
+    .text(function(d,i){
+      return d.text;
+    });
+  },
+  componentDidMount(){
+    var _this = this;
+    var eB1 = d3.select(".OrderTopo").append("svg").attr("width",500).attr("height",500);
+    //中心点
+    var centerX = eB1.attr("width")/2;
+    var centerY = eB1.attr("height")/2;
+    var circleNum = 9;//圆弧9等分
+    var centerR = 150;//圆半径
+    var otherData = [];
+    for(var i = 0;i < circleNum;i++){
+      var y = Math.sin( i*40*Math.PI/180 )*centerR + centerY;//对边 + 中心点宽
+      var x = Math.cos( i*40*Math.PI/180)*centerR + centerX;//临边 + 中心点高
+      otherData.push({x:x,y:y,text:i});
+    }
+
+    this.addOtherTag(otherData,eB1);
+    // //先添加线 节点可以覆盖多余的线条 ==================================================
+    // var oLine1 = eB1.append("g").attr("style","cursor:pointer");
+    // oLine1.selectAll("line").data(otherData).enter().append("line").attr("stroke","#ccc")
+    // .attr("x1",function(d,i){
+    //   return d.x;
+    // })
+    // .attr("y1",function(d,i){
+    //   return d.y;
+    // })
+    // .attr("x2",centerX)
+    // .attr("y2",centerY)
+    // //线中小方块
+    // oLine1.selectAll("rect").data(otherData).enter().append("rect").attr("fill","#999").attr("width",20).attr("height",20).attr("rx",5)
+    // .attr("x",function(d,i){
+    //   return (d.x + centerX)/2 -10;
+    // })
+    // .attr("y",function(d,i){
+    //   return (d.y + centerY)/2 - 10;
+    // })
+    // //线中文字
+    // oLine1.selectAll("text").data(otherData).enter().append("text").attr("fill","white").attr("font-size",20).attr("height",20).attr("text-anchor","middle")
+    // .attr("x",function(d,i){
+    //   return (d.x + centerX)/2;
+    // })
+    // .attr("y",function(d,i){
+    //   return (d.y + centerY)/2 +8;
+    // }).text("?")
+    //
+    // //添加其他点=================================================================
+    // var oG = eB1.append("g").attr("style","cursor:pointer");
+    // oG.selectAll("circle").data(otherData).enter()
+    // .append("circle")
+    // .attr("fill","white").attr("stroke","red").attr("stroke-width","1")
+    // .attr("cx",function(d,i){
+    //   return d.x;
+    // })
+    // .attr("cy",function(d,i){
+    //   return d.y;
+    // })
+    // .attr("r",25)
+    // .on("mouseover",function(d,i){
+    //   // alert();
+    //   _this.startMove( this , 30 , 40 );
+    //   //找到对应的线
+    //
+    // })
+    // .on("mouseout",function(d,i){
+    //   _this.startMove( this, 40 , 30 );
+    // });
+    //
+    // oG.selectAll("text").data(otherData).enter()
+    // .append("text")
+    // .attr("font-size",20).attr("text-anchor","middle")
+    // .attr("x",function(d,i){
+    //   return d.x;
+    // })
+    // .attr("y",function(d,i){
+    //   return d.y+8;
+    // })
+    // .text(function(d,i){
+    //   return d.text;
+    // })
+
+    //添加中心点
+    var cG = eB1
+    .append("g").attr("style","cursor:pointer");
+    cG.append("circle").attr("cx",centerX).attr("cy",centerY).attr("r",40).attr("fill","white").attr("stroke","red").attr("stroke-width","1");
+    cG.append("text").attr("x",centerX).attr("y",centerY+8).attr("font-size",20).attr("text-anchor","middle").text("中心点");
+
+  },
+  //动画效果  可以学习javascript中的运动
+  startMove(obj,r1,r2){
+    var nowR = r1;
+    var overR = r2;
+    obj.speed = 0;
+    clearInterval(obj.timer);
+    obj.timer = setInterval(function(){
+
+      obj.speed += (overR - nowR)/6;
+      obj.speed *= 0.9;
+
+      if( Math.abs(overR - nowR)<=1 && Math.abs(obj.speed)<=1 ){
+        clearInterval(obj.timer);
+        obj.setAttribute( 'r' , overR );
+      }
+      else{
+        nowR += obj.speed;
+        obj.setAttribute( 'r' , nowR );
+      }
+
+    },30);
+  },
+  render(){
+    return(
+      <div>
+        <div className="OrderTopo">
+        </div>
+      </div>
+    )
+  }
+})
 var ExampleB = React.createClass({
   componentDidMount(){
     //实现简单的动态效果
@@ -159,6 +354,7 @@ var ExampleB = React.createClass({
       <div>
         <div className="exampleB1" style={{float:"left"}}></div>
         <ExampleTopo />
+        <OrderTopo />
         <div className="exampleB2">
           <p>Hello</p>
           <p>World</p>
